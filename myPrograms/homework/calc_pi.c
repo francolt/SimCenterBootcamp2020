2cd41d682f68c99574422af6d0fd2b36c1591df9
Franklin Olaya <folaya@login3.frontera.tacc.utexas.edu>
2020-08-18 13:53:12 -0500

    ss

--- myPrograms/homework/calc_pi.c
+++ myPrograms/homework/calc_pi.c
@@ -3,14 +3,16 @@
 static int long numSteps = 100000;
 
 int main() {
-  double pi=0; double time = 0; double var = 0; double x = 0;
+  double pi=0; double time = 0; double stepx = 1./numSteps; double x = stepx*0.5;
   clock_t begin = clock();
 
   for (int i=0; i<numSteps; i++){
-    var = var + i/numSteps;
-    x = (var + var + i/numSteps)/2;  
-    pi = pi + 4.0 / (1.0 + x*x)*(1/numSteps);
-      }
+    pi +=4./(1+x*x);
+    x += stepx;
+    }
+
+  pi *= stepx;
+
   clock_t end = clock();
   time += (double)(end - begin) / CLOCKS_PER_SEC;
   printf("PI=%f, duration:%f ms\n", pi, time);
